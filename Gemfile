b6bd060afeae35e158693ed30a0ce3b1055e3cdd
remy taglang <remy.taglang@gmail.com>
2017-04-27 16:48:53 +0200

    last part is not done

--- Gemfile
+++ Gemfile
@@ -1,9 +1,10 @@
 source "https://rubygems.org"
 
-gem "sinatra", "2.0.0.rc1"
-gem "sinatra-contrib", "2.0.0.rc1"
-gem "pry-byebug"
 gem "better_errors"
 gem "binding_of_caller"
-gem 'sqlite3'
+gem "pry-byebug"
 gem 'rack-test'
+gem "sinatra", "2.0.0.rc1"
+gem "sinatra-contrib", "2.0.0.rc1"
+gem 'sqlite3'
+
--- app.rb
+++ app.rb
@@ -3,14 +3,45 @@ require "sinatra/reloader" if development?
 require "sqlite3"
 
 DB = SQLite3::Database.new(File.join(File.dirname(__FILE__), 'db/jukebox.sqlite'))
+DB.results_as_hash = true
 
 get "/" do
-  results = DB.execute("SELECT name FROM artists ORDER BY name")
-  artists = results.flatten
-  # TODO: Gather all artists to be displayed on home page
-  erb :home, locals: { artists: artists }
+  @artists = DB.execute("SELECT * FROM artists")
+  erb :home
+end
+
+get "/artists/:id" do
+  @artist = DB.execute("SELECT * FROM artists WHERE artists.id = #{params[:id]}")
+  @albums = DB.execute("SELECT * FROM albums WHERE artist_id = #{params[:id]}")
+
+  @count_by_genre = DB.execute("
+  SELECT count(tracks.id) AS count, genres.name
+  FROM tracks
+  JOIN genres ON genres.id = tracks.genre_id
+  JOIN albums ON albums.id = tracks.album_id
+  WHERE albums.artist_id = #{params[:id]}
+  GROUP BY genres.name
+  ORDER BY count DESC")
+  erb :artist
+end
+
+get "/albums/:id" do
+  @album = DB.execute("SELECT title FROM albums WHERE id=#{params[:id]}").first
+  @tracks = DB.execute("SELECT * FROM tracks WHERE album_id=#{params[:id]}")
+
+  erb :album
   # Will render views/home.erb file (embedded in layout.erb)
 end
+
+get "/albums/:id" do
+  @album = DB.execute("SELECT title FROM albums WHERE id=#{params[:id]}").first
+  @tracks = DB.execute("SELECT * FROM tracks WHERE album_id=#{params[:id]}")
+
+  erb :album
+  # Will render views/home.erb file (embedded in layout.erb)
+end
+
+
 
 # Then:
 # 1. Create an artist page with all the albums. Display genres as well
