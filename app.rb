f77c4e80e80664768628147204bccc65d951953e
remy taglang <remy.taglang@gmail.com>
2017-04-27 14:50:28 +0200

    display artists done

--- app.rb
+++ app.rb
@@ -5,13 +5,16 @@ require "sqlite3"
 DB = SQLite3::Database.new(File.join(File.dirname(__FILE__), 'db/jukebox.sqlite'))
 
 get "/" do
-  'Hello world!'
+  results = DB.execute("SELECT name FROM artists ORDER BY name")
+  artists = results.flatten
   # TODO: Gather all artists to be displayed on home page
-  # erb :home # Will render views/home.erb file (embedded in layout.erb)
+  erb :home, locals: { artists: artists }
+  # Will render views/home.erb file (embedded in layout.erb)
 end
 
 # Then:
 # 1. Create an artist page with all the albums. Display genres as well
 # 2. Create an album pages with all the tracks
 # 3. Create a track page, and embed a Youtube video (you might need to hit Youtube API)
+#
 
